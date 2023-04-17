5f66c83f89d61584644b955c6ca99a6cccd11450
Jarlarex <94379463+Jarlarex@users.noreply.github.com>
2023-04-17 18:23:56 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.src = 'https://platform.twitter.com/widgets.js';
+script.async = true;
+script.charset = 'utf-8';
+document.head.appendChild(script);
